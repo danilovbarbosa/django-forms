4fac71fa42ac52dccbefcd6e472e1ed869730e7e
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-02-05 16:43:52 -0300

    Incrementando atributos no PassagemForm.

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 
 from django import forms
-from django.forms import SelectDateWidget
+from django.forms import SelectDateWidget, Textarea
 from tempus_dominus.widgets import DatePicker, TimePicker, DateTimePicker
 
+from passagens.classe_viagem import tipos_de_classe
 
 class PassagemForms(forms.Form):
     origem = forms.CharField(label='Origem', max_length=100)
@@ -11,3 +12,9 @@ class PassagemForms(forms.Form):
     data_ida = forms.DateField(label='Ida', widget=DatePicker())
     data_volta = forms.DateField(label='Volta', widget=SelectDateWidget(empty_label="Nothing"))
     data_pesquisa = forms.DateField(label='Data da pesquisa', disabled=True, initial=datetime.today)
+    classe_viagem = forms.ChoiceField(label='Classes do vôo', choices=tipos_de_classe)
+    informacoes = forms.CharField(
+        label='Informações extras',
+        max_length=200,
+        widget=Textarea()
+    )
