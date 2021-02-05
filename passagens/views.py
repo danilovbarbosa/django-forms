d73c0fd6f9c20c5794d07501b388efa1adec8331
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-02-05 10:46:16 -0300

    Criado um base.html. index.html e minha_consulta.html extends de base.html.

--- passagens/views.py
+++ passagens/views.py
@@ -9,3 +9,13 @@ def index(request):
         'form': form,
     }
     return render(request, 'index.html', context=context)
+
+
+def revisao_consulta(request):
+
+    if request.method == 'POST':
+        formulario = PassagemForms(request.POST)
+        context = {
+            'formulario': formulario,
+        }
+        return render(request, 'minha_consulta.html', context=context)
