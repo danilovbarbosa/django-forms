79b629a9de3c2224288e1c59d54a8d4a086027d8
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-02-05 16:32:49 -0300

    Adicionando ao form PassagemForm o DatePicker e SelectDateWidget para campos diferentes. Adicionando para  mesma classe o atributo data_pesquisa. Ajusando os templates para o devido suporte.

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,7 +1,13 @@
+from datetime import datetime
+
 from django import forms
+from django.forms import SelectDateWidget
+from tempus_dominus.widgets import DatePicker, TimePicker, DateTimePicker
+
 
 class PassagemForms(forms.Form):
     origem = forms.CharField(label='Origem', max_length=100)
     destino = forms.CharField(label='Destino', max_length=100)
-    data_ida = forms.DateField(label='Ida')
-    data_volta = forms.DateField(label='Volta')
+    data_ida = forms.DateField(label='Ida', widget=DatePicker())
+    data_volta = forms.DateField(label='Volta', widget=SelectDateWidget(empty_label="Nothing"))
+    data_pesquisa = forms.DateField(label='Data da pesquisa', disabled=True, initial=datetime.today)
