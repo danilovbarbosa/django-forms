6f12b6502ed96dfc2eedbae00d599e15bda5c4ea
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-02-07 14:58:58 -0300

    Finalizado o formulário. PessoaForm foi adicionado  a view e ao template.

--- passagens/views.py
+++ passagens/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
-from passagens.forms import PassagemForms
+from passagens.forms import PassagemForms, PessoaForms
 
 
 def index(request):
     form = PassagemForms()
+    pessoas_form = PessoaForms()
     context = {
         'form': form,
+        'pessoas_form': pessoas_form,
     }
     return render(request, 'index.html', context=context)
 
@@ -14,13 +16,17 @@ def index(request):
 def revisao_consulta(request):
     if request.method == 'POST':
         form = PassagemForms(request.POST)
+        pessoa_form = PessoaForms(request.POST)
+
         if form.is_valid():
             context = {
                 'form': form,
+                'pessoa_form': pessoa_form,
             }
             return render(request, 'minha_consulta.html', context=context)
         else:
             context = {
                 'form': form,
+                'pessoa_form': pessoa_form,
             }
             return render(request, 'index.html', context=context)
